1c528aa213a5adcb43b40941cac1384ba571c402
Durga Yamini <kodamanchiliyamini@gmail.com>
2022-04-24 11:44:13 +0530

    counter with NgRx

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hello-world'`, () => {
+  it(`should have as title 'e-commerce-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello-world');
+    expect(app.title).toEqual('e-commerce-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('hello-world app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('e-commerce-app app is running!');
   });
 });
---
+++ src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { decrement, increment, reset } from '../state/counter.actions';
+
+@Component({
+  selector: 'app-counter-buttons',
+  templateUrl: './counter-buttons.component.html',
+  styleUrls: ['./counter-buttons.component.css']
+})
+export class CounterButtonsComponent implements OnInit {
+  // @Output() inc = new EventEmitter<void>();
+  // @Output() decr = new EventEmitter<void>();
+  // @Output() reset = new EventEmitter<void>();
+  constructor(private store: Store<{counter: {counter: number}}>) { }
+
+  ngOnInit(): void {
+  }
+  onIncrement(){
+    // this.inc.emit();
+    this.store.dispatch(increment());
+  }
+  onDecrement(){
+    // this.decr.emit();
+    this.store.dispatch(decrement());
+  }
+  onReset(){
+    // this.reset.emit();
+    this.store.dispatch(reset());
+  }
+
+}
