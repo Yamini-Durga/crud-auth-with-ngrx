b9837f84d35cc6d8ec716c4f86913d2d85ad87b5
Durga Yamini <kodamanchiliyamini@gmail.com>
2022-05-28 21:27:33 +0530

    update, delete operations

--- src/app/posts/posts-list/state/posts.reducers.ts
+++ src/app/posts/posts-list/state/posts.reducers.ts
@@ -1,5 +1,5 @@
 import { createReducer, on } from "@ngrx/store";
-import { addPostAction } from "./posts.actions";
+import { addPostAction, deletePostAction, updatePostAction } from "./posts.actions";
 import { initialState } from "./posts.state";
 
 const _postsReducer = createReducer(initialState,
@@ -10,7 +10,23 @@ const _postsReducer = createReducer(initialState,
             ...state,
             posts: [...state.posts, post]
         }
-    }));
+    }),
+    on(updatePostAction, (state, action) => {
+        let updatedPosts = state.posts.map(p => 
+            p.id === action.post.id ? action.post : p);
+        return {
+            ...state,
+            posts: updatedPosts
+        }
+    }),
+    on(deletePostAction, (state, action) => {
+        let updatedPosts = state.posts.filter(p => p.id !== action.id);
+        return {
+            ...state,
+            posts: updatedPosts
+        }
+    })
+);
 
 export function postsReducer(state, action){
     return _postsReducer(state, action);
--- src/app/posts/posts-list/state/posts.selectors.ts
+++ src/app/posts/posts-list/state/posts.selectors.ts
@@ -4,4 +4,8 @@ import { PostsState } from "./posts.state";
 export const getPostsState = createFeatureSelector<PostsState>('posts');
 export const getPosts = createSelector(getPostsState, state => {
     return state.posts
-})
+});
+
+export const getPostById = createSelector(getPostsState, (state, props) => {
+    return state.posts.find(p => p.id === props.id);
+});
